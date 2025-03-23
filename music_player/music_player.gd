57eabee5538929ab986567a83374fd4a8fdb84e8
nathaliajoana <nathaliajoana@hotmail.com>
2025-03-23 16:48:26 -0300

    update play and random buttons to textured

--- music_player/music_player.gd
+++ music_player/music_player.gd
@@ -7,14 +7,13 @@ var song_position : int = 0
 var stage_songs : Array = ["Cosy Coffee Shop", "Fading Memories", "Flavoured Water"]
 
 @onready var music_player : AudioStreamPlayer = $MusicPlayer
-@onready var play_button: Button = $VBoxContainer/HBoxContainer/PlayButton
+@onready var play_button: TextureButton = $VBoxContainer/HBoxContainer/PlayButton
+@onready var random_button: TextureButton = $VBoxContainer/HBoxContainer/RandomButton
 @onready var back_button: Button = $VBoxContainer/HBoxContainer/BackButton
 @onready var next_button: Button = $VBoxContainer/HBoxContainer/NextButton
 
-
 #region Main functions
 func _ready() -> void:
-	play_button.set_text("Play") # TODO: change icon using TexturedButton
 	back_button.set_disabled(true) # TODO: enable after first song finishes
 	next_button.set_disabled(true) # TODO: do we want this to be enabled at first?
 #endregion
@@ -23,14 +22,12 @@ func _ready() -> void:
 
 func _on_play_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		play_button.set_text("Pause") 
 		AudioManager.play_music(stage_songs, song_position, false, random)
 		next_button.set_disabled(false)
 	else:
-		play_button.set_text("Play")
 		AudioManager.set_pause(true)	
 	
-func _on_randomize_button_toggled(toggled_on: bool) -> void:
+func _on_random_button_toggled(toggled_on: bool) -> void:
 	random = toggled_on
 
 func _on_loop_button_toggled(toggled_on: bool) -> void:
@@ -45,9 +42,7 @@ func _on_next_button_pressed() -> void:
 
 func _on_back_button_pressed() -> void:
 	song_position = stage_songs.find(AudioManager.current_song) - 1
-	if song_position < stage_songs.size() - 1: 
-		#song_position = 0
-		back_button.set_disabled(true) # TODO: use seek?
+	if song_position < stage_songs.size() - 1: back_button.set_disabled(true) # TODO: use seek?
 	AudioManager.play_music(stage_songs, song_position, false, random, true)
 	if !play_button.is_pressed(): play_button.set_pressed_no_signal(true)
 #endregion
